7c82012713d64888bd775422dc3cd049abbce21d
0xAB <abgar.abrahamyan@picsart.com>
2022-03-18 19:50:38 +0400

    first commit

---
+++ src/main/java/com/example/electronicmedicalrecord/ElectronicMedicalRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicmedicalrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicMedicalRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicMedicalRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/electronicmedicalrecord/ElectronicMedicalRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicmedicalrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicMedicalRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
